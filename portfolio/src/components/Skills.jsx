9707315811e2b9c66a35a072003d6f80812d35f0
KaranSinghG <karansinghg@ymail.com>
2026-01-02 21:10:44 +0530

    Add Skills component and integrate it into App

---
+++ portfolio/src/components/Skills.jsx
@@ -0,0 +1,15 @@
+function Skills() {
+    return (
+        <>
+            <h2>Skills</h2>
+            <ul>
+                <li>Java, Spring Boot, REST APIs</li>
+                <li>React, JavaScript, HTML, CSS</li>
+                <li>SQL</li>
+                <li>Python, Machine Learning (Basics)</li>
+            </ul>
+        </>
+    );
+}
+
+export default Skills;
