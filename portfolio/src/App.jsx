ebcc0dc4c7b1a0f6237e18b1e85d2983d42cd6d1
KaranSinghG <karansinghg@ymail.com>
2026-01-03 19:02:32 +0530

    Add Projects component and integrate it into App

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,6 @@
 import About from './components/About'
 import Header from './components/Header'
+import Projects from './components/Projects'
 import Skills from './components/Skills'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <About />
       <Skills />
+      <Projects />
     </>
   )
 }
---
+++ portfolio/src/components/Projects.jsx
@@ -0,0 +1,23 @@
+function Projects() {
+    return (
+        <>
+            <h2>Projects</h2>
+
+            <div >
+                <h3>AEBAS</h3>
+                <p>Aadhaar Enabled Biometric Attendance System</p>
+                <p><b>Tech:</b> Python, Raspberry Pi, Fingerprint module</p>
+                <a href="#">GitHub</a>
+            </div>
+
+            <div>
+                <h3>Chatbot using Dialogflow</h3>
+                <p>Simple React app with Dialogflow integration</p>
+                <p><b>Tech:</b> React, Dialogflow</p>
+                <a href="#">GitHub</a>
+            </div>
+        </>
+    );
+}
+
+export default Projects;
