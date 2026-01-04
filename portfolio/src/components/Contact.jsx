a1cebbe83df1e34f01b943dedde91459f49a7bbd
KaranSinghG <karansinghg@ymail.com>
2026-01-04 21:02:15 +0530

    Add Contact component and integrate it into App

---
+++ portfolio/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+function Contact() {
+    return (
+        <>
+            <h2>Contact</h2>
+            <p>Email: karansinghg@ymail.com</p>
+            <p>Phone: +91-9953722838</p>
+            <p><a href="https://www.linkedin.com/in/karan-singh-24265a268/">LinkedIn</a></p>
+            <p><a href="https://github.com/KaranSinghG">GitHub</a></p>
+        </>
+    );
+}
+
+export default Contact;
