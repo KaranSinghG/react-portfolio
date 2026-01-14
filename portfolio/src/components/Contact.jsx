1811d19ef77ac094cba09ba0976a6cf76acafcf1
KaranSinghG <karansinghg@ymail.com>
2026-01-14 23:52:24 +0530

    Refactor Contact component structure; update CSS for improved layout and styling

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -1,12 +1,18 @@
+import './css/contact.css';
+
 function Contact() {
     return (
-        <>
-            <h2>Contact</h2>
-            <p>Email: karansinghg@ymail.com</p>
-            <p>Phone: +91-9953722838</p>
-            <p><a href="https://www.linkedin.com/in/karan-singh-24265a268/">LinkedIn</a></p>
-            <p><a href="https://github.com/KaranSinghG">GitHub</a></p>
-        </>
+        <div className="contact">
+            <div className="contact-container">
+                <h2>Contact</h2>
+                <div className="contact-details">
+                    <p>Email: karansinghg@ymail.com</p>
+                    <p>Phone: +91-9953722838</p>
+                    <p><a href="https://www.linkedin.com/in/karan-singh-24265a268/">LinkedIn</a></p>
+                    <p><a href="https://github.com/KaranSinghG">GitHub</a></p>
+                </div>
+            </div>
+        </div>
     );
 }
 
